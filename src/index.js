2191441070266125a6eb983b3264ada37fd17911
Alejandro Garcia <painisprep@gmail.com>
2025-07-14 23:56:23 -0400

    refactor: move basic UI functioning from screen controller to index

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ import "./styles/content.css";
 import "./styles/dialog-add-new-todo.css";
 import { screenController } from "./modules/screen-controller.js";
 
-screenController();
+const collapseSideBar = document.getElementById("collapse-side-bar");
+const showSideBar = document.getElementById("show-side-bar");
+const sideBar = document.getElementById("side-bar");
+const content = document.getElementById("content");
 
+collapseSideBar.addEventListener("click", () => {
+  sideBar.classList.toggle("hidden");
+  content.style.width = "100vw";
+  showSideBar.classList.toggle("hidden");
+});
+showSideBar.addEventListener("click", () => {
+  sideBar.classList.toggle("hidden");
+  content.style.width = "calc(100vw - 280px)";
+  showSideBar.classList.toggle("hidden");
+});
+
+screenController();
