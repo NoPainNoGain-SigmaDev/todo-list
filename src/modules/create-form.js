b47baaa6fce046dd4307d5bf522dad5eed25c4aa
Alejandro Garcia <painisprep@gmail.com>
2025-07-15 16:35:49 -0400

    refactor: delegating project creation and user updates to the corresponding modules

--- src/modules/create-form.js
+++ src/modules/create-form.js
@@ -1,107 +1,161 @@
-import { createEl } from "./dom-tools";
+import { createEl, autoResize, closeDialog } from "./dom-tools";
+import {user} from "../index.js";
 
 export function createForm() {
+    ///Form for new to dos
   const formAddNewTodo = () => {
+    //Form components
+    const titleInput = createEl("input", {
+      id: "title",
+      type: "text",
+      placeholder: "Wash the dog, it stinks...",
+      required: true,
+      autofocus: true,
+    });
+
+    const textArea = createEl("textarea", {
+      id: "description",
+      rows: "1",
+      placeholder: "Description",
+    }); 
+
+    const dateInput = createEl("input", {
+      type: "date",
+      id: "date",
+    });
+
+    const prioritySelect = createEl(
+      "select",
+      { className: "priority-select", id: "priority-select" },
+      [
+        createEl("option", { value: "default", textContent: "Priority" }),
+        createEl("option", {
+          className: "select-priority-low",
+          textContent: "Low",
+        }),
+        createEl("option", {
+          className: "select-priority-medium",
+          textContent: "Medium",
+        }),
+        createEl("option", {
+          className: "select-priority-high",
+          textContent: "High",
+        }),
+      ]
+    );
+
+    const closeBtn = createEl("input", {
+      type: "submit",
+      id: "dialog-close",
+      value: "Close",
+      formNoValidate: true,
+    });
+
+    const addBtn = createEl("input", {
+      type: "submit",
+      id: "form-add-todo",
+      value: "Add ToDo",
+    });
+
+    const projectSelect = createEl(
+      "select",
+      { className: "project-select", id: "project-select" },
+      [
+        createEl("option", {
+          value: "default",
+          textContent: "Things ToDo",
+        }),
+        createEl("option", { textContent: "Low" }),
+        createEl("option", { textContent: "Medium" }),
+        createEl("option", { textContent: "High" }),
+      ]
+    );
+
+    // Final form element
     const form = createEl("form", { id: "form" }, [
       createEl("div", { className: "form-top" }, [
         createEl("fieldset", { className: "form-level-1" }, [
-          createEl("input", {
-            id: "title",
-            type: "text",
-            placeholder: "Wash the dog, it stinks...",
-            required: true,
-          }),
-          createEl("textarea", {
-            id: "description",
-            rows: "1",
-            placeholder: "Description",
-          }),
+          titleInput,
+          textArea,
         ]),
         createEl("fieldset", { className: "form-level-2" }, [
-          createEl("input", { type: "date", id: "date" }),
-          createEl(
-            "select",
-            { className: "priority-select", id: "priority-select" },
-            [
-              createEl("option", { value: "default", textContent: "Priority" }),
-              createEl("option", {
-                className: "select-priority-low",
-                textContent: "Low",
-              }),
-              createEl("option", {
-                className: "select-priority-medium",
-                textContent: "Medium",
-              }),
-              createEl("option", {
-                className: "select-priority-high",
-                textContent: "High",
-              }),
-            ]
-          ),
+          dateInput,
+          prioritySelect,
         ]),
       ]),
       createEl("div", { className: "form-bottom" }, [
-        createEl("div", { className: "form-actions" }, [
-          createEl("input", {
-            type: "submit",
-            id: "dialog-close",
-            value: "Close",
-            formNoValidate: true,
-          }),
-          createEl("input", {
-            type: "submit",
-            id: "form-add-todo",
-            value: "Add ToDo",
-            autofocus: true,
-          }),
-        ]),
-        createEl(
-          "select",
-          { className: "project-select", id: "project-select" },
-          [
-            createEl("option", {
-              value: "default",
-              textContent: "Things ToDo",
-            }),
-            createEl("option", { textContent: "Low" }),
-            createEl("option", { textContent: "Medium" }),
-            createEl("option", { textContent: "High" }),
-          ]
-        ),
+        createEl("div", { className: "form-actions" }, [closeBtn, addBtn]),
+        projectSelect,
       ]),
     ]);
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      dialog.close();
+      dialog.innerHTML = "";
+      console.log("close");
+    });
+
+    textArea.addEventListener("input", () => autoResize(textArea));
 
     return form;
   };
+  //Form for new projects
   const formAddNewProject = () => {
+    //Form elements
+    const titleInput = createEl("input", {
+      id: "title",
+      type: "text",
+      placeholder: "Shopping List",
+      required: true,
+      autofocus: true,
+    });
+
+    const closeButton = createEl("button", {
+      type: "button",
+      id: "dialog-close",
+      value: "Close",
+      formNoValidate: true,
+      textContent: "Close"
+    });
+
+    const addButton = createEl("input", {
+      type: "submit",
+      id: "form-add-todo",
+      value: "Add New",
+      
+    });
+
+    // Final form
     const form = createEl("form", { id: "form" }, [
       createEl("div", { className: "form-top" }, [
-        createEl("fieldset", { className: "form-level-1" }, [
-          createEl("input", {
-            id: "title",
-            type: "text",
-            placeholder: "Shopping List",
-            required: true,
-          }),
-        ]),
+        createEl("fieldset", { className: "form-level-1" }, [titleInput]),
       ]),
       createEl("div", { className: "form-bottom" }, [
         createEl("div", { className: "form-actions" }, [
-          createEl("input", {
-            type: "submit",
-            id: "dialog-close",
-            value: "Close",
-            formNoValidate: true,
-          }),
-          createEl("input", {
-            type: "submit",
-            id: "form-add-todo",
-            value: "Add New",
-            autofocus: true,
-          }),
+          closeButton,
+          addButton,
         ]),
       ]),
     ]);
+
+    closeButton.addEventListener("click", ()=>{
+        closeDialog();
+        return;
+    });
+
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const newProjectName = document.getElementById("title").value;
+      user.newProject(newProjectName);
+
+      closeDialog();
+
+      user.self();
+
+    });
 
     return form;
   };
@@ -111,45 +165,41 @@ export function createForm() {
     const date = todo.getDueDate();
     const priority = todo.getPriority();
     const id = todo.getId();
-    
+
     const projects = [];
 
-    avaiableProjects.forEach(project => {
-        const projectOption = createEl(
-            "option",
-            {
-                textContent : project.getTitle(),
-            }
-        );
+    avaiableProjects.forEach((project) => {
+      const projectOption = createEl("option", {
+        textContent: project.getTitle(),
+      });
 
-        project.push(projectOption);
+      project.push(projectOption);
     });
 
-
-    const form = createEl("form", { id: "form", dataSet : {todoId : id}  }, [
+    const form = createEl("form", { id: "form", dataSet: { todoId: id } }, [
       createEl("div", { className: "form-top" }, [
         createEl("fieldset", { className: "form-level-1" }, [
           createEl("input", {
             id: "title",
             type: "text",
-            value : title,
+            value: title,
             placeholder: "Wash the dog, it stinks...",
             required: true,
           }),
           createEl("textarea", {
             id: "description",
-            textContent : description,
-            rows : "1",
+            textContent: description,
+            rows: "1",
             placeholder: "Description",
           }),
         ]),
         createEl("fieldset", { className: "form-level-2" }, [
-          createEl("input", { type: "date", id: "date", value : date,}),
+          createEl("input", { type: "date", id: "date", value: date }),
           createEl(
             "select",
             { className: "priority-select", id: "priority-select" },
             [
-              createEl("option", { value: "default", textContent: priority, }),
+              createEl("option", { value: "default", textContent: priority }),
               createEl("option", {
                 className: "select-priority-low",
                 textContent: "Low",
@@ -177,17 +227,17 @@ export function createForm() {
           }),
           createEl("input", {
             type: "submit",
-            className : "hidden",
+            className: "hidden",
             id: "form-add-todo",
             value: "Update",
-            disabled : true,
+            disabled: true,
             autofocus: true,
           }),
         ]),
         createEl(
           "select",
           { className: "project-select", id: "project-select" },
-          projects,
+          projects
         ),
       ]),
     ]);
--- src/modules/screen-controller.js
+++ src/modules/screen-controller.js
@@ -1,75 +1,97 @@
-import { createUser } from "./create-user";
 import { dialogController } from "./dialog-controller";
+import { createEl, clear } from "./dom-tools";
+import {user} from "../index.js";
 
+export function screenController() {
+  
+ 
+  
 
-export function screenController(pastUser) {
-  //new user or stored user data
-  const user = pastUser? pastUser : createUser();
+  //admin controll window.user = user;
 
   //demo
-  window.user = user;
-  console.log(user.userName());
-  user.addToProject(user.newTodo("TEST 1"));
-  user.newProject("CHECKLIST PROJECT");
-  const newP = user.getProjects()[1].getId();
-  user.addToProject(user.newTodo(), newP);
-  const newT = user.getProjects()[1].getProjectContent()[0];
-  newT.updateDescription("Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industry's standard dummy textever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
-  newT.updatePriority("high");
-  newT.updateDueDate("2025-12-25");
-  newT.addToChecklist(user.newChecklistItem("Checklist item"));
-  newT.addToChecklist(user.newChecklistItem("Checklist item"));
-  newT.addToChecklist(user.newChecklistItem("Checklist item"));
-  newT.addToChecklist(user.newChecklistItem("Checklist item"));
-  newT.addToChecklist(user.newChecklistItem("Checklist item"));
-  newT.addToChecklist(user.newChecklistItem("Checklist item"));
+//   console.log(user.userName());
+//   user.addToProject(user.newTodo("TEST 1"));
+//   user.newProject("CHECKLIST PROJECT");
+//   const newP = user.getProjects()[1].getId();
+//   user.addToProject(user.newTodo(), newP);
+//   const newT = user.getProjects()[1].getProjectContent()[0];
+//   newT.updateDescription("Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industry's standard dummy textever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+//   newT.updatePriority("high");
+//   newT.updateDueDate("2025-12-25");
+//   newT.addToChecklist(user.newChecklistItem("Checklist item"));
+//   newT.addToChecklist(user.newChecklistItem("Checklist item"));
+//   newT.addToChecklist(user.newChecklistItem("Checklist item"));
+//   newT.addToChecklist(user.newChecklistItem("Checklist item"));
+//   newT.addToChecklist(user.newChecklistItem("Checklist item"));
+//   newT.addToChecklist(user.newChecklistItem("Checklist item"));
 
-  user.self();
+//   user.self();
   //end demo
 
   //DOM elements
   const addNewTodo = document.getElementById("add-new-todo");
   const addNewProject = document.getElementById("add-new-project");
-  const collapseSideBar = document.getElementById("collapse-side-bar");
-  const showSideBar = document.getElementById("show-side-bar");
-  const sideBar = document.getElementById("side-bar");
   const dialog = document.getElementById("dialog");
+  const projectsNav = document.getElementById("nav-content");
   const content = document.getElementById("content");
   
   const dialogCont = dialogController();
 
 
- 
+  const updateProjectNav = () => {
+    const createProjectElement = (project) => {
+        return createEl("div", {className : "project hover-effect", id : project.getId()}, [
+            createEl("div", {className : "project-info"}, [
+                createEl("i", {className : "fa-solid fa-folder-closed"}),
+                createEl("h2", { textContent : project.getProjectName()}),
+            ]),
+            createEl("div", {className : "project-tools"}, [
+                createEl("button", {className : "project-delete"}, [
+                    createEl("i", {className : "fa-solid fa-trash"}),
+                ]),
+            ]),
+        ]);
+    };
+
+    clear(projectsNav);
+
+    const avaiableProjects = user.getProjects();
+
+    avaiableProjects.forEach(project=>{
+        projectsNav.appendChild(createProjectElement(project));
+    });
+  };
+
+
 
   addNewTodo.addEventListener("click", () => {
     dialogCont.dialogAddNewTodo();
     dialog.showModal();
   });
 
-  
-
   addNewProject.addEventListener("click", () => {
     dialogCont.dialogAddNewProject();
     dialog.showModal();
+
+    dialog.addEventListener("close", updateProjectNav);
+    dialog.addEventListener("cancel", ()=>{clear(dialog)});
   });
 
-  collapseSideBar.addEventListener("click", ()=>{
-    sideBar.classList.toggle("hidden");
-    content.style.width = "100vw";
-    showSideBar.classList.toggle("hidden");
-  });
-   showSideBar.addEventListener("click", ()=>{
-        sideBar.classList.toggle("hidden");
-        content.style.width = "calc(100vw - 280px)";
-        showSideBar.classList.toggle("hidden");
-    });
+  projectsNav.addEventListener("click", (e)=>{
+    
+    const currentSelectedProject = projectsNav.querySelector(".selected-project");
+    if(currentSelectedProject){
+        currentSelectedProject.classList.remove("selected-project");
+    }
 
+    const newSelectedProject = e.target.closest(".project");
+    newSelectedProject.classList.toggle("selected-project");
+    console.log(user.getProject(newSelectedProject.id))
+  });
+
+
+
+
+  updateProjectNav();
 };
-
-
-// dialogCont.dialogExpandTodo(newT);
-//     const textarea = document.getElementById("description");
-//     textarea.addEventListener("input", () => autoResize(textarea));
-
-//     dialog.showModal();
-//     autoResize(textarea)
