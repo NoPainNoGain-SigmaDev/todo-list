d229965813e8c6df9e4dd812ce5c863769aed928
Alejandro Garcia <painisprep@gmail.com>
2025-07-14 23:25:31 -0400

    refactor: move logic to screen controller

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles/global.css";
+import "./styles/side-bar.css";
+import "./styles/content.css";
+import "./styles/dialog-add-new-todo.css";
+import { screenController } from "./modules/screen-controller.js";
+
+screenController();
+
