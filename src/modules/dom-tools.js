6a29459652a8c90f7f6c9bc79f342577de190a63
Alejandro Garcia <painisprep@gmail.com>
2025-07-14 23:21:20 -0400

    feat: add DOM utility functions

---
+++ src/modules/dom-tools.js
@@ -0,0 +1,13 @@
+//DOM manipulation functions used through modules
+function createEl(tag, props = {}, children = []) {
+  const el = Object.assign(document.createElement(tag), props);
+  children.forEach(child => el.appendChild(child));
+  return el;
+}
+
+function clearDOM() {
+  const contentContainer = document.getElementById("content");
+  contentContainer.innerHTML = "";
+}
+
+export { createEl, clearDOM };
