662f71717c0fbfe134302af48175fa7734866fa7
Alejandro Garcia <painisprep@gmail.com>
2025-07-16 22:32:30 -0400

    feat: add demo projects and todos

--- src/index.js
+++ src/index.js
@@ -23,6 +23,36 @@ showSideBar.addEventListener("click", () => {
 
 //new user or stored user data SINGLETON USER
 export const user = createUser();
+//Demo
+user.newProject("Checklist");
+user.newProject("Shopping List");
+user.newProject("Homework");
+
+const p = user.getProjects();
+const id0 = p[0].getId(); // "Things ToDo" (already exists)
+const id1 = p[1].getId(); // Checklist
+const id2 = p[2].getId(); // Shopping List
+const id3 = p[3].getId(); // Homework
+
+user.addToProject(user.newTodo("Buy groceries", "Milk, eggs, chicken, rice", "2025-07-20", "medium"), id0);
+user.addToProject(user.newTodo("Fix the leaky faucet", "Call maintenance or get a wrench", "2025-07-21", "low"), id0);
+
+
+user.addToProject(user.newTodo("Pack phone charger"), id1);
+user.addToProject(user.newTodo("Turn off lights"), id1);
+user.addToProject(user.newTodo("Lock the door"), id1);
+
+
+user.addToProject(user.newTodo("Toilet paper"), id2);
+user.addToProject(user.newTodo("Shampoo"), id2);
+user.addToProject(user.newTodo("Apples"), id2);
+
+
+user.addToProject(user.newTodo("Write English essay", "Topic: Climate Change", "2025-07-22", "high"), id3);
+user.addToProject(user.newTodo("Study for math quiz", "Practice algebra problems", "2025-07-23", "medium"), id3);
+user.addToProject(user.newTodo("Read history chapter", "Chapter 4: Ancient Rome", "2025-07-24", "low"), id3);
+
+
 
 
 screenController();
